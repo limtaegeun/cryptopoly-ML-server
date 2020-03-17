9416a01f02aa93d20ee646052bc2265db7ee71cf
taegeun lim <imori333@gmail.com>
2020-03-17 22:40:42 +0900

    add predict route

--- app.py
+++ app.py
@@ -5,16 +5,19 @@
 # 	curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
 # Submit a request via Python:
 #	python simple_request.py
-import flask
+from flask import Flask, request, jsonify
 import numpy as np
 import io
 from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+import json
 from keras.models import model_from_json
-from sklearn.externals import joblib
+import joblib
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 model = None
 scaler = None
+
 def load_model():
     # load predict model
     global model
@@ -29,18 +32,33 @@ def load_model():
 def load_scaler():
     global scaler
     scaler = joblib.load('scaler.plk')
+    print("Loaded scaler from disk")
 
 def prepare_data(data_array):
     global scaler
     return scaler.transform(data_array)
 
+def origin_data(data_array):
+    global scaler
+    return scaler.inverse_transform(data_array)
+
 @app.route("/predict", methods=["POST"])
 def predict():
-    # if flask.request.method == "POST":
-        # if flask.request.body:
-        #     prepared_data = prepare_data()
-    return 'hi'
+    if request.method == "POST":
+        jsonData = json.loads(request.form['data'])
+        nparray = np.asarray(jsonData)
+        prepared_data = prepare_data(nparray)
+        reshaped_data = np.reshape(prepared_data, ( prepared_data.shape[0], prepared_data.shape[1],1))
+        print(reshaped_data)
+        predict = model.predict(reshaped_data)
+        print(predict)
+        return jsonify(predict.tolist())
 
 
 if __name__ == '__main__':
-    app.run()
+    print(("* Loading Keras model and Flask starting server..."
+           "please wait until server has fully started"))
+    load_model()
+    load_scaler()
+    app.run(debug=True)
+
